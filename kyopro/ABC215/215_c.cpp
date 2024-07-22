c41057d0075c49c84419638b6185bc4af602eaaf
Ren-YamadaRY <orenobentu@gmail.com>
2024-07-22 16:36:01 +0900

    update

--- kyopro/ABC215/215_c.cpp
+++ kyopro/ABC215/215_c.cpp
@@ -78,20 +78,60 @@ void listin(vector<T>& A, ll n) {
     }
 }
 
-string s;
-ll k;
+ll n, m, k, a;
+vector<sll> s(200009);
 
 int main() {
-    cin >> s >> k;
-    ll cnt = 0;
-    sort(all(s));
-    do {
-        cnt++;
-        if (cnt == k) {
-            cout << s << endl;
-            break;
+    cin >> n >> m;
+    repi (i, 1, m) {
+        cin >> k;
+        repi (j, 1, k) {
+            cin >> a;
+            s.at(i - 1).push(a);
         }
     }
-    while (next_permutation(all(s)));
+    queue<pll> q;
+    map<ll, ll> mp;
+    repi (i, 1, m) {
+        if (!s.at(i - 1).empty()) {
+            if (!mp.count(s.at(i - 1).top())) {
+                mp[s.at(i - 1).top()] = i - 1;
+            }
+            else {
+                q.push(make_pair(i - 1, mp[s.at(i - 1).top()]));
+            }
+        }
+    }
+    vll ans;
+    while (!q.empty()) {
+        pll p = q.front();
+        ll fst = p.first, snd = p.second;
+        q.pop();
+        ans.pb(s.at(fst).top());
+        s.at(fst).pop();
+        s.at(snd).pop();
+        if (!s.at(fst).empty()) {
+            if (!mp.count(s.at(fst).top())) {
+                mp[s.at(fst).top()] = fst;
+            }
+            else {
+                q.push(make_pair(fst, mp[s.at(fst).top()]));
+            }
+        }
+        if (!s.at(snd).empty()) {
+            if (!mp.count(s.at(snd).top())) {
+                mp[s.at(snd).top()] = snd;
+            }
+            else {
+                q.push(make_pair(snd, mp[s.at(snd).top()]));
+            }
+        }
+    }
+    if (sz(ans) == n) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
     return 0;
 }
