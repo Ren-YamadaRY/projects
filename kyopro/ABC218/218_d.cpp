caffab971aa125b05a1ca2a939a1f616995ffcfb
Ren-YamadaRY <orenobentu@gmail.com>
2024-07-24 18:18:58 +0900

    update

---
+++ kyopro/ABC218/218_d.cpp
@@ -0,0 +1,106 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using pll = pair<ll, ll>;
+using qll = queue<ll>; // push, front, pop, size, empty
+using pqll = priority_queue<ll>; // push, top, pop, size, empty
+using smallpqll = priority_queue<ll, vector<ll>, greater<ll>>;
+using dqll = deque<ll>; // push_back, push_front, front, back, pop_front, pop_back, at, size, empty
+using sll = stack<ll>; // push, top, pop, size, empty
+
+#define repi(i, s, n) for (ll i = (s); i <= (ll)(n); i++)
+#define repd(i, s, n) for (ll i = (s); i >= (ll)(n); i--)
+#define all(x) x.begin(),x.end()
+#define sz(x) ((ll)(x).size())
+#define pb push_back
+
+template<typename T>
+vector<T> unique(vector<T> a) {
+    sort(all(a));
+    a.erase(unique(all(a)), a.end());
+    return a;
+}
+template<typename T>
+void prv(vector<T> a) {
+    repi (i, 1, sz(a)) {
+        cout << a.at(i - 1) << " ";
+    }
+    cout << endl;
+}
+template<typename T>
+void prv(vector<vector<T>> a) {
+    repi (i, 1, sz(a)) {
+        repi (j, 1, sz(a.at(0))) {
+            cout << a.at(i - 1).at(j - 1) << " ";
+        }
+        cout << endl;
+    } 
+}
+template<typename T>
+vector<vector<T>> trans(vector<vector<T>> a) {
+    vector<vector<T>> a_trans(sz(a.at(0)), vector<T>(sz(a), 0));
+    repi(i, 1, sz(a.at(0))) {
+        repi(j, 1, sz(a)) {
+            a_trans.at(i - 1).at(j - 1) = a.at(j - 1).at(i - 1);
+        }
+    }
+    return a_trans;
+}
+template<typename T>
+void prl(vector<T> a) {
+    repi (i, 1, sz(a)) {
+        cout << a.at(i - 1) << " ";
+    }
+}
+template<typename T>
+void prvp(vector<pair<T, T>> a) {
+    repi (i, 1, sz(a)) {
+        cout << a.at(i - 1).first << " " << a.at(i - 1).second << endl;
+    }
+}
+template<typename T>
+void listin(vector<T>& A, ll n) {
+    repi (i, 1, n) {
+        T a;
+        cin >> a;
+        A.pb(a);
+    }
+}
+
+ll n, x, y;
+vector<pll> p;
+
+int main() {
+    cin >> n;
+    set<pll> s;
+    repi (i, 1, n) {
+        cin >> x >> y;
+        p.pb(make_pair(x, y));
+        s.insert(make_pair(x, y));
+    }
+    ll ans = 0;
+    repi (i, 1, n) {
+        repi (j, i + 1, n) {
+            if (p.at(i - 1).first != p.at(j - 1).first && p.at(i - 1).second != p.at(j - 1).second) {
+                pll t_1 = make_pair(p.at(i - 1).first, p.at(j - 1).second);
+                pll t_2 = make_pair(p.at(j - 1).first, p.at(i - 1).second);
+                if (s.count(t_1) && s.count(t_2)) {
+                    ans++;
+                }
+            }
+        }
+    }
+    cout << ans / 2 << endl;
+    return 0;
+}
