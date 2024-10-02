54d4a4714331999a459c532e420c0fb2b8338b5a
Ren-YamadaRY <orenobentu@gmail.com>
2024-10-02 18:06:29 +0900

    update

--- javascript/JSPractice/main.js
+++ javascript/JSPractice/main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const Person = {
+    name: "John",
+    age: 30,
+    address: {
+        city: "東京",
+        sipcode: "123-4567"
+    },
+    hobbies: ["読書", "映画鑑賞", "ゲーム"]
+};
+
+const greet = (person) => {
+    return `Hello, ${person.name}!`;
+}
+
+console.log(greet(Person));
